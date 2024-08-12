62eb52e95604e02c6f41cf29f64224b37e881f52
durgaprasad104 <128016655+durgaprasad104@users.noreply.github.com>
2024-08-12 08:23:29 +0530

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+function searchFunction() {
+    var query = document.getElementById("search-bar").value;
+    alert("Search functionality is not implemented. You searched for: " + query);
+}
