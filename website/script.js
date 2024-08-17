ff5baf69d5b546dc5718f44391943713e24e3f03
durgaprasad104 <128016655+durgaprasad104@users.noreply.github.com>
2024-08-17 12:56:45 +0530

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,11 +1,44 @@
 // script.js
 
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    const menuIcon = document.querySelector('.menu-icon');
-    menu.classList.toggle('active');
+// Select the menu icon and menu
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+// Add a click event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+    // Toggle the 'active' class on the menu icon
     menuIcon.classList.toggle('active');
+    
+    // Toggle the visibility of the menu
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
+
+
+function checkPassword() {
+    var password = document.getElementById("passwordInput").value;
+    var errorContainer = document.getElementById("errorContainer");
+
+    // Clear previous error message
+    errorContainer.textContent = '';
+
+
+    if (password === "12345") {
+        document.querySelector(".password-container").style.display = "none";
+        document.getElementById("projects-content").style.display = "block"; // Show the content
+    } else {
+        // Display error message
+            errorContainer.textContent = "Incorrect password. Please try again.";
+            document.getElementById("passwordInput").value = ''; // Clear the input field
+    }
 }
+// Hide content initially
+window.onload = function() {
+    document.getElementById("projects-content").style.display = "none";
+};
 
 function searchFunction() {
     // Add search functionality here
